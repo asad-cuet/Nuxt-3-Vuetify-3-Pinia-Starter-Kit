2ed76b515cfbfe5a6fd01175ec09f8797e824fdd
Asadul Islam Hamzah <68386760+asad-cuet@users.noreply.github.com>
2023-03-24 02:16:40 +0600

    all working

--- app.vue
+++ app.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-    <v-btn>Hello Sass Changes</v-btn>
+    <p>Count={{ counter.count }}</p>
+    <v-btn @click="counter.increment()">Increment</v-btn>
   </div>
 </template>
 
-
+<script setup>
+import {useCounterStore} from '@/stores/counter'
+const counter=useCounterStore()
+</script>
 <style lang="scss">
 @use './settings';
 </style>

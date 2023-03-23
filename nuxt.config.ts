87f02c365db7049fa9f6c7d7b8e9b1295f381ec4
Asadul Islam Hamzah <68386760+asad-cuet@users.noreply.github.com>
2023-03-24 01:56:47 +0600

    All installed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,20 +5,23 @@ import { createResolver } from '@nuxt/kit'
 const { resolve } = createResolver(import.meta.url)
 
 export default defineNuxtConfig({
-  css: [
-    'vuetify/lib/styles/main.sass',
-    '@mdi/font/css/materialdesignicons.min.css',
-  ],
-  build: {
-    transpile: ['vuetify'],
-  },
-  hooks: {
-    'vite:extendConfig': config => {
-      config.plugins.push(
-        vuetify({
-          styles: { configFile: resolve('./settings.scss') },
-        })
-      )
+    modules: [
+        '@pinia/nuxt',
+        ],
+    css: [
+        'vuetify/lib/styles/main.sass',
+        '@mdi/font/css/materialdesignicons.min.css',
+    ],
+    build: {
+        transpile: ['vuetify'],
     },
-  },
+    hooks: {
+        'vite:extendConfig': config => {
+        config.plugins.push(
+            vuetify({
+            styles: { configFile: resolve('./settings.scss') },
+            })
+        )
+        },
+    },
 })
